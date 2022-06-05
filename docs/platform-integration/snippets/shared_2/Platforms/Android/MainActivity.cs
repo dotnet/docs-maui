187208896a8f34f988544ce5f89442b994039ac1
Aden Earnshaw <github@adenearnshaw.com>
2022-06-05 10:07:33 +0100

    Adding IntentFilter Action[] constructor parameter
    IntentFilter requires an Actions array as the first parameter. Adding this, with the `ActionAppAction` declared in `Microsoft.Maui.ApplicationModel.Platform.Intent` class.

--- docs/platform-integration/snippets/shared_2/Platforms/Android/MainActivity.cs
+++ docs/platform-integration/snippets/shared_2/Platforms/Android/MainActivity.cs
@@ -7,7 +7,8 @@ namespace PlatformIntegration;
 
 //<intent_filter_1>
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-[IntentFilter(Categories = new[] { global::Android.Content.Intent.CategoryDefault })]
+[IntentFilter(new[] { Platform.Intent.ActionAppAction },
+              Categories = new[] { global::Android.Content.Intent.CategoryDefault })]
 public class MainActivity : MauiAppCompatActivity
 //</intent_filter_1>
 {
