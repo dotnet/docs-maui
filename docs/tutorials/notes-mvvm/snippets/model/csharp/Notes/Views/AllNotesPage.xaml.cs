8da7d590c980e1d11b3a089879589249d29658bc
Andy (Steve) De George <67293991+adegeo@users.noreply.github.com>
2023-01-13 17:04:32 -0800

    Tutorial 2 - Implement the MVVM pattern in the notes app (#1128)
    * Init for diff
    
    * Upgrade
    
    * viewmodel article done
    
    * fix full snippets
    
    * updates after doing full run of article
    
    * fix highlighted lines
    
    * restructure; add notifications
    
    * updates
    
    * dates
    
    * fix image paths
    
    * Move to shared snippet; remove notification
    
    * checkpoint
    
    * missing file
    
    * more tweaks
    
    * more tweaks
    
    * more fixes
    
    * final part
    
    * tweaks
    
    * tweak
    
    * Remove file
    
    * tweak
    
    * add links to samples
    
    * outro
    
    * Update TOC.yml
    
    * Update index.yml
    
    * Update index.yml
    
    * Apply suggestions from code review
    
    Co-authored-by: David Britch <davidbritch@users.noreply.github.com>
    
    * feedback
    
    * Apply suggestions from code review
    
    Co-authored-by: David Britch <davidbritch@users.noreply.github.com>
    
    * feedback
    
    * Apply suggestions from code review
    
    Co-authored-by: David Britch <davidbritch@users.noreply.github.com>
    
    * add mvvm article
    
    * image
    
    * fix link
    
    * Fix interface
    
    * update first tutorial with ios icons
    
    * feedback
    
    * add link from 1st tutorial to 2nd
    
    Co-authored-by: David Britch <davidbritch@users.noreply.github.com>

---
+++ docs/tutorials/notes-mvvm/snippets/model/csharp/Notes/Views/AllNotesPage.xaml.cs
@@ -0,0 +1,19 @@
+namespace Notes.Views;
+
+public partial class AllNotesPage : ContentPage
+{
+	public AllNotesPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Add_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void notesCollection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+
+    }
+}
