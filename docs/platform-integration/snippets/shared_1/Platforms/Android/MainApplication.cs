f654d108ce2105ada207b56b2eefba9872c48293
David Britch <davidbritch@users.noreply.github.com>
2023-04-18 12:15:10 +0100

    Update MediaPicker permissions for Android 13 (#1439)
    * Update permissions for Android 13.
    
    * Edits.
    
    * Remove hard tabs.
    
    * Edits.
    
    * Edits.

--- docs/platform-integration/snippets/shared_1/Platforms/Android/MainApplication.cs
+++ docs/platform-integration/snippets/shared_1/Platforms/Android/MainApplication.cs
@@ -7,11 +7,14 @@ using Android.Runtime;
 
 //<media_picker>
 // Needed for Picking photo/video
-[assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage, MaxSdkVersion = 32)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadMediaAudio)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadMediaImages)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadMediaVideo)]
 
 // Needed for Taking photo/video
-[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
 [assembly: UsesPermission(Android.Manifest.Permission.Camera)]
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage, MaxSdkVersion = 32)]
 
 // Add these properties if you would like to filter out devices that do not have cameras, or set to false to make them optional
 [assembly: UsesFeature("android.hardware.camera", Required = true)]
